bfd290af34ee164f8196d39094f336232b37e470
Jonathan Otto <jonathan.otto@gmail.com>
2017-01-10 22:32:03 -0800

    More bots and add note on _escaped_fragment_

--- variables.go
+++ variables.go
@@ -5,6 +5,19 @@ import "regexp"
 var cfSchemeRegex = regexp.MustCompile("\"scheme\":\"(http|https)\"")
 
 var crawlerUserAgents = [...]string{
+	// probably better to use _escaped_fragment_ rather
+	// than risk cloaking penalties for the big 3
+	// read more here: https://developers.google.com/webmasters/ajax-crawling/docs/specification
+	// (note: while the ajax-crawling has been deprecated, it might still be useful as a balance between
+	//        reducing risk of cloaking penalties and still guaranteeing a full render for google.
+	//        because despite their claims, they still don't always wait around for all ajax requests
+	//        to complete
+
+	// uncomment the big 3 at your own risk
+	// "googlebot",
+	// "yahoo",
+	// "bingbot",
+
 	"baiduspider",
 	"facebookexternalhit",
 	"twitterbot",
@@ -15,6 +28,19 @@ var crawlerUserAgents = [...]string{
 	"showyoubot",
 	"outbrain",
 	"pinterest",
+	"pinterest/0.",
 	"developers.google.com/+/web/snippet",
 	"slackbot",
+	"vkShare",
+	"W3C_Validator",
+	"redditbot",
+	"Applebot",
+	"WhatsApp",
+	"flipboard",
+	"tumblr",
+	"bitlybot",
+	"SkypeUriPreview",
+	"nuzzel",
+	"Discordbot",
+	"Google Page Speed",
 }
